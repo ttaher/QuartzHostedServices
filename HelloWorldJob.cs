bee4bfa9fe8cca9b9a91b1f83a9168e0b62354af
ttaher <talat.abdelbaky@gmail.com>
2019-06-29 22:39:16 +0200

    inital commit

---
+++ HelloWorldJob.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzHostedServices
+{
+    public class HelloWorldJob : IJob
+    {
+        private readonly ILogger<HelloWorldJob> _logger;
+
+        public HelloWorldJob(ILogger<HelloWorldJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("Hello world! at: {0}", DateTime.UtcNow);
+            //_logger.LogInformation("Hello world!");
+            return Task.CompletedTask;
+        }
+    }
+}
